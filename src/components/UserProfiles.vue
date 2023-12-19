f177fe0c08940210535d3171a6d9b9eed86cc32b
Mcrivers0517 <wangrhofficial@gmail.com>
2023-12-20 00:11:24 +0800

    完成了基本的页面布局

---
+++ src/components/UserProfiles.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header" height>
+        <div class="logo">
+          <img
+            src="../assets/processed_logo.png"
+            alt="Logo"
+            class="logo-image"
+          />
+        </div>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#242632"
+          text-color="#fff"
+          active-text-color="#f0b90b"
+          style="margin-left: 30px"
+        >
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3">消息中心</el-menu-item>
+          <el-menu-item index="4">订单管理</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-container class="content-container">
+        <el-aside class="aside" width="200px">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#242632"
+            text-color="#fff"
+            active-text-color="#f0b90b"
+            width="10px"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main class="main"></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.common-layout {
+  padding: 20px;
+  background-color: #f0f0f0;
+}
+
+.header {
+  background-color: #242632;
+  color: white;
+  height: 8vh;
+  text-align: center;
+  border-radius: 12px 12px 0 0;
+  display: flex;
+}
+
+.aside {
+  background-color: #242632;
+  color: white;
+  height: 84vh;
+  border-radius: 0 0 0 12px;
+}
+
+.main {
+  background-color: #242632;
+  padding: 10px;
+  height: 84vh;
+  border-radius: 0 0 12px 0;
+}
+.content-container {
+  flex: 1;
+}
+.el-menu {
+  border: 0 !important;
+}
+.logo-image {
+  width: 45px;
+  height: auto;
+  margin-top: 8px;
+  margin-left: 15px;
+}
+</style>
