8677e2226d75efa5b126fb92cebba51b8912483e
gaetos <1786304306@qq.com>
2023-12-20 23:16:38 +0800

    提交了Springboot启动文件

---
+++ FutureTradingSystem-Springboot/src/main/java/com/FTS/FutureTradingSystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.FTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureTradingSystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureTradingSystemSpringbootApplication.class, args);
+	}
+
+}
