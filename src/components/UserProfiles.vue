3a727e579f0a5c304cd8cb6a5fbdd9bde44ea4ba
Mcrivers0517 <wangrhofficial@gmail.com>
2023-12-20 22:47:39 +0800

    完成的页面布局的设计

--- src/components/UserProfiles.vue
+++ src/components/UserProfiles.vue
@@ -19,86 +19,73 @@
           active-text-color="#f0b90b"
           style="margin-left: 30px"
         >
-          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-menu-item index="1">主页</el-menu-item>
           <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
+            <template slot="title">订单管理</template>
+            <el-menu-item index="2-1">当前委托</el-menu-item>
+            <el-menu-item index="2-2">历史委托</el-menu-item>
           </el-submenu>
-          <el-menu-item index="3">消息中心</el-menu-item>
-          <el-menu-item index="4">订单管理</el-menu-item>
+          <el-menu-item index="3">行情</el-menu-item>
+          <el-menu-item index="4">消息中心</el-menu-item>
         </el-menu>
       </el-header>
       <el-container class="content-container">
-        <el-aside class="aside" width="200px">
-          <el-menu
-            default-active=""
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            background-color="#181a20"
-            text-color="#848e8c"
-            active-text-color="#f0b90b"
-            width="10px"
-          >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
         <el-main class="main">
           <div class="user-avatar">
             <div class="block">
-              <el-avatar shape="square" :size="65" :src="squareUrl"></el-avatar>
+              <el-avatar shape="square" :size="65" :src="defaultAvatar"></el-avatar>
             </div>
           </div>
           <div class="user-info">
             <div class="user-name">
-              <span>这里是用户名</span>
+              <span>Mcrivers</span>
             </div>
             <div class="user-id">
-              <span>这里是用户id</span>
+              <span>202103150618</span>
             </div>
           </div>
           <div class="main-content" id="main-content-1">
-            <p>这是一条测试信息</p>
+            <div class="assets-container">
+              <span class="assets">总资产估值</span>
+              <div class="assets-values">
+                <span class="assets-value">429.1181194</span>
+                <span class="assets-currency">RMB</span>
+              </div>
+              <div class="profit-and-loss">
+                <span class="profit-and-loss-title">今日盈亏</span>
+                <span class="profit-and-loss-value">+4.67%(1.55%)</span>
+              </div>
+            </div>
           </div>
           <div class="main-content" id="main-content-2">
-            <p>这是一条测试信息2</p>
+            <div>
+              <span class="posi">持仓情况</span>
+              <el-table
+                :data="tableData"
+                class="posi-table"
+                :cell-style="tableRowClassName"
+              >
+                <el-table-column prop="type" label="品种" :width="80"> </el-table-column>
+                <el-table-column prop="buy/sell" label="买卖">
+                </el-table-column>
+                <el-table-column prop="Position" label="持仓">
+                </el-table-column>
+                <el-table-column prop="Floating Profit/Loss" label="浮动盈亏">
+                </el-table-column>
+                <el-table-column prop="Profit/Loss Ratio" label="盈亏比率">
+                </el-table-column>
+                <el-table-column prop="Average Opening Price" label="开仓均价">
+                </el-table-column>
+                <el-table-column prop="Current Price" label="现价">
+                </el-table-column>
+                <el-table-column
+                  prop="Latest Transaction Time"
+                  label="最新成交时间"
+                  :width="175"
+                >
+                </el-table-column>
+              </el-table>
+            </div>
           </div>
         </el-main>
       </el-container>
@@ -109,14 +96,54 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      tableData: [
+        {
+          type: "A",
+          "buy/sell": "Buy",
+          Position: 100,
+          "Floating Profit/Loss": 150.25,
+          "Profit/Loss Ratio": 0.075,
+          "Average Opening Price": 50.75,
+          "Current Price": 52.0,
+          "Latest Transaction Time": "2023-12-19 14:30:45",
+        },
+        {
+          type: "B",
+          "buy/sell": "Sell",
+          Position: 20000,
+          "Floating Profit/Loss": -500.8,
+          "Profit/Loss Ratio": -0.025,
+          "Average Opening Price": 1.1205,
+          "Current Price": 1.118,
+          "Latest Transaction Time": "2023-12-19 15:45:20",
+        },
+        {
+          type: "C",
+          "buy/sell": "Buy",
+          Position: 500,
+          "Floating Profit/Loss": 75.4,
+          "Profit/Loss Ratio": 0.03,
+          "Average Opening Price": 150.8,
+          "Current Price": 152.0,
+          "Latest Transaction Time": "2023-12-19 16:15:10",
+        },
+      ],
+      defaultAvatar:"../assets/default-avatar.png",
+    };
+  },
+  methods: {
+    tableRowClassName() {
+      return "background: #181a20";
+    },
   },
 };
 </script>
 
 <style scoped>
 .common-layout {
-  background-color: #eaecef;
+  background-color: #2767c6;
+  border-radius: 12px 12px 12px 12px;
 }
 
 .header {
@@ -139,7 +166,7 @@ export default {
   background-color: #181a20;
   padding: 10px;
   height: 89vh;
-  border-radius: 0 0 12px 0;
+  border-radius: 0 0 12px 12px;
   display: flex;
   justify-content: center;
   flex-direction: column;
@@ -158,36 +185,40 @@ export default {
   margin-left: 15px;
 }
 #main-content-1 {
-  position: relative;
+  /* position: relative; */
   border: 1px solid #2b3139;
   padding: 20px; /* 增加边框与内容之间的间距 */
-  width: 56%;
+  position: absolute;
+  left: 50%;
+  width: 50%;
   height: 20%;
-  top: 3%;
-  left: 10%;
+  top: 32%;
   border-radius: 12px;
   color: #eaecef;
+  transform: translate(-50%, -50%);
 }
 #main-content-2 {
-  position: relative;
+  /* position: relative; */
   border: 1px solid #2b3139;
   padding: 20px; /* 增加边框与内容之间的间距 */
-  width: 56%;
+  position: absolute;
+  width: 50%;
   height: 40%;
-  top: 8%;
-  left: 10%;
+  top: 72%;
   border-radius: 12px;
   color: #eaecef;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 .user-avatar {
   position: absolute;
   top: 5%;
-  left: 12%;
+  left: 25%;
 }
 .user-info {
   position: absolute;
-  top: 6%;
-  left: 20%;
+  top: 7%;
+  left: 31%;
   color: #eaecef;
   display: flex;
   align-items: center;
@@ -195,5 +226,72 @@ export default {
 }
 .user-id {
   margin-top: 8px;
+}
+.posi {
+  font-size: 20px;
+  font-weight: 600;
+  color: #eaecef;
+}
+.posi-table {
+  width: 100%;
+  background-color: transparent !important;
+}
+.posi-table::v-deep .el-table th.el-table__cell,
+::v-deep .el-table th,
+::v-deep .el-table tr {
+  color: #efebea;
+  background-color: transparent !important;
+  border-bottom-color: #2b3139 !important;
+}
+::v-deep .posi-table td,
+::v-deep .posi-table th.is-leaf {
+  border-bottom: 1px solid #2b3139;
+}
+.el-table--border::after,
+.el-table--group::after,
+.el-table::before {
+  background-color: #2b3139;
+}
+.el-table tbody tr:hover > td {
+  background-color: red;
+}
+.assets-container {
+  display: flex;
+  flex-direction: column; /* 设置垂直排列 */
+}
+.assets {
+  font-size: 20px;
+  font-weight: 600;
+}
+.assets-value {
+  font-size: 30px;
+  font-weight: 600;
+}
+.assets-values {
+  margin-top: 1.6%;
+  display: flex;
+}
+.assets-currency {
+  font-size: 13px;
+  font-weight: 600;
+  margin-top: 2.5%;
+  margin-left: 1%;
+}
+.profit-and-loss {
+  display: flex;
+}
+
+.profit-and-loss-title {
+  margin-top: 1.6%;
+  font-size: 16px;
+  font-weight: 200;
+}
+
+.profit-and-loss-value {
+  margin-top: 1.6%;
+  font-size: 15px;
+  font-weight: 600;
+  margin-left: 1%;
+  color: #f6465d;
 }
 </style>
