dde2556ae7b1a5c9b3c0b26817c205b6e5118c6c
gaetos <1786304306@qq.com>
2023-12-21 14:25:19 +0800

    springboot，启动！

---
+++ FutureTradingSystem-Springboot/src/test/java/com/example/futuretradingsystemspringboot/FutureTradingSystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futuretradingsystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureTradingSystemSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
