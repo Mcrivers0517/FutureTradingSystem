d9dc9d6ad491ac9a19849b7a82b8666e5f8bdfd0
720ok <1193866996@qq.com>
2023-12-22 12:48:24 +0800

    后端注册修改

--- src/main/java/cn/edu/zjut/fts/controller/RegisterController.java
+++ src/main/java/cn/edu/zjut/fts/controller/RegisterController.java
@@ -5,23 +5,21 @@ import cn.edu.zjut.fts.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-public class UserController {
+public class RegisterController {
     @Autowired
     private UserMapper userMapper;
 
 //查询所有用户
-    @GetMapping("/user")
-    public  List query(){
+//    @GetMapping("/user")
+//    public  List query(){
+//
+//        List<User> list = userMapper.selectall();
+//        System.out.println(list);
+//        return list;
+//    }
 
-        List<User> list = userMapper.selectall();
-        System.out.println(list);
-        return list;
-    }
-
-@PostMapping("/user")
+@PostMapping("/register")
 public String insert(@RequestParam("username") String username,
                      @RequestParam("password") String password,
                      @RequestParam("confirmPassword") String confirmPassword) {
