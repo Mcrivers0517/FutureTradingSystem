183657afb70057f6e507633ad55de415a197b24f
Mcrivers0517 <wangrhofficial@gmail.com>
2023-12-22 15:30:06 +0800

    增加了对ECharts的支持

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import * as echarts from "echarts";
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.prototype.$echarts = echarts;
 
 new Vue({
   render: h => h(App),
