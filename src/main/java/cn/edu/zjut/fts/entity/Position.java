5bf9592de3bdc9ab5dc52f0b0c5936d58329fc17
gaetos <1786304306@qq.com>
2023-12-22 13:33:12 +0800

    position部分操作

---
+++ src/main/java/cn/edu/zjut/fts/entity/Position.java
@@ -0,0 +1,27 @@
+package cn.edu.zjut.fts.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("positiontable")
+public class Position {
+
+    private String id;
+    private String username;
+    private String BS;
+    private int pos;
+    private int c_Pro;
+    private double r_Pro;
+    private int f_Price;
+    private int n_Price;
+    private Date n_Time;
+}
