1392b16e5b4df203465fa0fa23e228c636aa856e
gaetos <1786304306@qq.com>
2023-12-24 01:47:38 +0800

    数据库更新

--- springboot/sql/U_Table.sql
+++ springboot/sql/U_Table.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS User,PositionTable,DelegateTable;
+DROP TABLE IF EXISTS User,PositionTable,DelegateTable,TransactionTable;
 
 #建立用户表
 CREATE TABLE IF NOT EXISTS User(
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS User(
 
 #建立持仓表
 CREATE TABLE IF NOT EXISTS PositionTable(
-    /*品种*/id VARCHAR(10) PRIMARY KEY,
+    /*品种*/id VARCHAR(10),
     /*持仓人*/username VARCHAR(100) ,
-    /*买或卖*/BS VARCHAR(10),
+    /*买或卖*/bs VARCHAR(10),
     /*当前持仓量*/POS INT,
     /*浮动盈亏*/C_pro INT,
     /*盈亏比率*/R_pro DECIMAL(4,2),
@@ -23,15 +23,24 @@ CREATE TABLE IF NOT EXISTS PositionTable(
     /*username作为外键*/FOREIGN KEY PositionTable(username) REFERENCES User(username)
 );
 
+#建立成交记录表
+CREATE TABLE IF NOT EXISTS TransactionTable(
+    /*品种*/id VARCHAR(10),
+    /*持仓人*/username VARCHAR(100) ,
+    /*买或卖,开或平*/bs VARCHAR(10),
+    /*成交价格*/price INT,
+    /*手续费*/premium INT,
+    /*利润*/pro INT,
+    /*成交时间*/time DATETIME
+);
 
 #建立委托记录表
 CREATE TABLE IF NOT EXISTS DelegateTable(
-    /*品种*/id VARCHAR(10) PRIMARY KEY,
+    /*品种*/id VARCHAR(10),
     /*持仓人*/username VARCHAR(100) ,
     /*属性*/att VARCHAR(10),
     /*状态*/status TINYINT(1),
     /*委托数量*/num INT,
-    /*委托价格*/price INT,
-    /*委托时间*/time DATETIME,
-    /*username作为外键*/FOREIGN KEY (username) REFERENCES User(username)
+    /*委托价格*/D_price INT,
+    /*委托时间*/D_time DATETIME
 );
---
+++ springboot/sql/U_test.sql
@@ -0,0 +1,23 @@
+﻿-- 插入用户数据
+INSERT INTO User (username, password, avatarUrl) VALUES
+('user1', 'password1', 'avatar1.jpg'),
+('user2', 'password2', 'avatar2.jpg'),
+('user3', 'password3', 'avatar3.jpg');
+
+-- 插入持仓数据
+INSERT INTO PositionTable (id, username, bs, POS, C_pro, R_pro, F_price, N_price, N_time, Delivery) VALUES
+('A001', 'user1', 'Buy', 100, 50, 0.05, 100, 110, '2023-01-01 12:00:00', '2023-02-01 12:00:00'),
+ ('B001', 'user2', 'Sell', 50, -20, -0.02, 150, 140, '2023-01-02 14:30:00', '2023-02-02 14:30:00'),
+('C001', 'user3', 'Buy', 75, 30, 0.03, 200, 210, '2023-01-03 10:45:00', '2023-02-03 10:45:00');
+
+-- 插入成交记录数据
+INSERT INTO transactiontable (id, username, bs, price, premium, pro,time) VALUES
+('A001', 'user1', 'Buy', 110, 5, 1,'2023-01-01 12:05:00'),
+('B001', 'user2', 'Sell', 140, 3,1, '2023-01-02 14:35:00'),
+('C001', 'user3', 'Buy', 210, 8,1, '2023-01-03 10:50:00');
+
+-- 插入委托记录数据
+INSERT INTO DelegateTable (id, username, att, status, num, D_price, D_time) VALUES
+('A001', 'user1', 'Buy', 1, 50, 105, '2023-01-01 11:55:00'),
+('B001', 'user2', 'Sell', 0, 30, 145, '2023-01-02 14:25:00'),
+('C001', 'user3', 'Buy', 1, 25, 205, '2023-01-03 10:40:00');
